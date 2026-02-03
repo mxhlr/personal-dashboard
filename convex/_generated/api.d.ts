699f31a41376861010930b8554fed26fbb087a74
Mx <michael.hueller@icloud.com>
2026-02-03 20:01:33 +0200

    [Checkpoint] NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_YXNzdXJl...

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,35 +8,37 @@
  * @module
  */
 
-import type * as myFunctions from "../myFunctions.js";
-import type * as todos from "../todos.js";
-
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
 
+declare const fullApi: ApiFromModules<{}>;
+
 /**
- * A utility for referencing Convex functions in your app's API.
+ * A utility for referencing Convex functions in your app's public API.
  *
  * Usage:
  * ```js
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{
-  myFunctions: typeof myFunctions;
-  todos: typeof todos;
-}>;
-declare const fullApiWithMounts: typeof fullApi;
-
 export declare const api: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "public">
 >;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
 export declare const internal: FilterApi<
-  typeof fullApiWithMounts,
+  typeof fullApi,
   FunctionReference<any, "internal">
 >;
 
