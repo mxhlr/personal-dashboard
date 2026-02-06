4ae206e6c39ac410767fef006ddea0eb3cc702d9
Mx <michael.hueller@icloud.com>
2026-02-06 23:35:23 +0200

    [Checkpoint] ES IST ÜBERHAUPT GARNICHTS GEÄNDERT. GARNICHTS. VE...

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,25 +12,25 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/logoappicon.png',
+        src: '/icon.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/logoappicon.png',
+        src: '/icon.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/logoappicon.png',
+        src: '/icon.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/logoappicon.png',
+        src: '/icon.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
