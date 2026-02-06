59a7e744b214f21b12c7777264b31b6b967e5b02
Mx <michael.hueller@icloud.com>
2026-02-06 23:04:20 +0200

    Add PWA auth & update mechanism

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-  cacheId: "v5-icons-update",
+  cacheId: "real-rise-v6",
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/.*\.convex\.cloud\/.*/i,
